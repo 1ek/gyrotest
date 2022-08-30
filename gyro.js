55ce171ca372b87cd94f28d78549d0aedb887399
1ek <100698589+1ekk@users.noreply.github.com>
2022-08-30 09:59:03 +0300

    Add files via upload

--- gyro.js
+++ gyro.js
@@ -1,14 +1,58 @@
 console.log("Hello world")
-const axis = document.querySelectorAll('.axis')
-let gyroscope = new Gyroscope({frequency: 60});
+let axis = document.querySelectorAll('.axis')
 
-gyroscope.addEventListener('reading', e => {
-    console.log("Ang vel X = " + gyroscope.x)
-    console.log("Ang vel Y = " + gyroscope.y)
-    console.log("Ang vel Z = " + gyroscope.z)
-    axis[0].textContent = gyroscope.x
-    axis[1].textContent = gyroscope.y
-    axis[2].textContent = gyroscope.z
+    if (
+      DeviceMotionEvent &&
+      typeof DeviceMotionEvent.requestPermission === "function"
+    ) {
+      DeviceMotionEvent.requestPermission();
+    }
+
+window.addEventListener("devicemotion", e => {
+    axis[0].innerHTML = e.rotationRate.beta
+    axis[1].innerHTML = e.rotationRate.gamma
+    axis[2].innerHTML = e.rotationRate.alpha
 });
 
-gyroscope.start();
+
+// function handleOrientation(event) {
+//     updateFieldIfNotNull('Orientation_a', event.alpha);
+//     updateFieldIfNotNull('Orientation_b', event.beta);
+//     updateFieldIfNotNull('Orientation_g', event.gamma);
+//     incrementEventCount();
+//   }
+  
+//   function incrementEventCount(){
+//     let counterElement = document.getElementById("num-observed-events")
+//     let eventCount = parseInt(counterElement.innerHTML)
+//     counterElement.innerHTML = eventCount + 1;
+//   }
+  
+//   function updateFieldIfNotNull(fieldName, value, precision=10){
+//     if (value != null)
+//       document.getElementById(fieldName).innerHTML = value.toFixed(precision);
+//   }
+  
+//   function handleMotion(event) {
+//     updateFieldIfNotNull('Gyroscope_z', event.rotationRate.alpha);
+//     updateFieldIfNotNull('Gyroscope_x', event.rotationRate.beta);
+//     updateFieldIfNotNull('Gyroscope_y', event.rotationRate.gamma);
+//     incrementEventCount();
+//   }
+  
+//   let is_running = false;
+//   let demo_button = document.getElementById("start_demo");
+//   demo_button.onclick = function(e) {
+//     e.preventDefault();
+    
+//     // Request permission for iOS 13+ devices
+//     if (
+//       DeviceMotionEvent &&
+//       typeof DeviceMotionEvent.requestPermission === "function"
+//     ) {
+//       DeviceMotionEvent.requestPermission();
+//     }
+    
+
+
+  
