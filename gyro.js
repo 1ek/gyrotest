762b2f13725dc1bf6f01273ea31669bd9c226bc7
1ek <100698589+1ekk@users.noreply.github.com>
2022-08-30 10:28:35 +0300

    Add files via upload

--- gyro.js
+++ gyro.js
@@ -1,21 +1,39 @@
 console.log("Hello world")
-const axis = document.querySelectorAll('.axis')
+// const axis = document.querySelectorAll('.axis')
 const btn = document.querySelector('.starter')
+let isRunning = false 
+
+function ifNotNull(id, value, amount = 5) {
+    if (value != null) {
+        document.getElementById(id).value.toFixed(amount)
+    }
+}
 
 function motionHandler(e) {
-    axis[0].innerHTML = e.rotationRate.beta
-    axis[1].innerHTML = e.rotationRate.gamma
-    axis[2].innerHTML = e.rotationRate.alpha
+    ifNotNull('x', e.rotationRate.beta)
+    ifNotNull('y', e.rotationRate.gamma)
+    ifNotNull('z', e.rotationRate.alpha)
+    // axis[0].innerHTML = e.rotationRate.beta.toFixed(5)
+    // axis[1].innerHTML = e.rotationRate.gamma.toFixed(5)
+    // axis[2].innerHTML = e.rotationRate.alpha.toFixed(5)
 }
 
 btn.addEventListener('click', e => {
     e.preventDefault();
     console.log("Pressed")
-    window.addEventListener("devicemotion", motionHandler)
+    if (isRunning) {
+        btn.innerText = "START"
+        isRunning = false
+        window.removeEventListener("devicemotion", motionHandler)
+    } else {
+        isRunning = true
+        btn.innerText = "STOP"
+        window.addEventListener("devicemotion", motionHandler)
+    }
+
     if (DeviceMotionEvent && typeof DeviceMotionEvent.requestPermission === "function") {
         DeviceMotionEvent.requestPermission();
     }
-    
 })
     
 
